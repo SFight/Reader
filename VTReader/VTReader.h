2035821e7a3a54307b37a87ac176841ccc9ccf45
宋金杰 <772326244@qq.com>
2018-04-10 10:24:28 +0800

    Initial Commit

---
+++ VTReader/VTReader.h
@@ -0,0 +1,19 @@
+//
+//  VTReader.h
+//  VTReader
+//
+//  Created by JinJie Song on 2018/4/10.
+//  Copyright © 2018年 JinJie Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VTReader.
+FOUNDATION_EXPORT double VTReaderVersionNumber;
+
+//! Project version string for VTReader.
+FOUNDATION_EXPORT const unsigned char VTReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VTReader/PublicHeader.h>
+
+
