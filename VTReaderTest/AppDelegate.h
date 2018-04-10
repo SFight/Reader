193dca878d7e68515a7d50c7502592245852034f
宋金杰 <772326244@qq.com>
2018-04-10 10:26:32 +0800

    Initial Commit

---
+++ VTReaderTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VTReaderTest
+//
+//  Created by JinJie Song on 2018/4/10.
+//  Copyright © 2018年 JinJie Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VTReaderTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VTReaderTest
+//
+//  Created by JinJie Song on 2018/4/10.
+//  Copyright © 2018年 JinJie Song. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
