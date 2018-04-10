462a557fd360dc63e605f7d9115ca0eaf32709df
宋金杰 <772326244@qq.com>
2018-04-10 10:56:41 +0800

    添加测试按钮以及调用

--- VTReaderTest/ViewController.m
+++ VTReaderTest/ViewController.m
@@ -19,12 +19,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(10, 100, 300, 50);
+    button.backgroundColor = [UIColor redColor];
+    [button addTarget:self action:@selector(onClickButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - SEL
+- (void)onClickButton:(UIButton *)button
+{
+    NSError *error;
+    NSString *srcPath = [[NSBundle mainBundle] pathForResource:@"三言二拍（插图版）" ofType:@"epub"];
+    NSString *dstPath = [[NSString documentsPath] stringByAppendingPathComponent:@"三言二拍.epub"];
+    [[NSFileManager defaultManager] copyItemAtPath:srcPath toPath:dstPath error:&error];
+    NSURL *path = [NSURL fileURLWithPath:[[NSString documentsPath] stringByAppendingPathComponent:@"三言二拍.epub"]];
+    
+    [[VTReaderManager sharedInstance] openReaderWithPath:path type:VT_READER_TYPE_EPUB inViewController:self onDismiss:^BOOL(UIViewController *viewController) {
+        
+        return YES;
+    }];
 }
 
 
