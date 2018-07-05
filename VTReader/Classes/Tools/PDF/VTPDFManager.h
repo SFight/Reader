bb1dabef703a5c3dafb4ba620ba6e7db7502cd8b
vtears <772326244@qq.com>
2018-07-05 10:49:10 +0800

    pdf阅读解析控制类

---
+++ VTReader/Classes/Tools/PDF/VTPDFManager.h
@@ -0,0 +1,13 @@
+//
+//  VTPDFManager.h
+//  VTReader
+//
+//  Created by JinJie Song on 2018/4/10.
+//  Copyright © 2018年 JinJie Song. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VTPDFManager : NSObject
+
+@end
