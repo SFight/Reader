636c25270cda3c3e533abb9ee9e93a248624ff38
vtears <772326244@qq.com>
2018-07-05 10:54:17 +0800

    修改测试epub为食品安全

--- VTReaderTest/ViewController.m
+++ VTReaderTest/ViewController.m
@@ -37,10 +37,10 @@
 - (void)onClickButton:(UIButton *)button
 {
     NSError *error;
-    NSString *srcPath = [[NSBundle mainBundle] pathForResource:@"三言二拍（插图版）" ofType:@"epub"];
-    NSString *dstPath = [[NSString documentsPath] stringByAppendingPathComponent:@"三言二拍.epub"];
+    NSString *srcPath = [[NSBundle mainBundle] pathForResource:@"1037191" ofType:@"epub"];
+    NSString *dstPath = [[NSString documentsPath] stringByAppendingPathComponent:@"睿智企业家.epub"];
     [[NSFileManager defaultManager] copyItemAtPath:srcPath toPath:dstPath error:&error];
-    NSURL *path = [NSURL fileURLWithPath:[[NSString documentsPath] stringByAppendingPathComponent:@"三言二拍.epub"]];
+    NSURL *path = [NSURL fileURLWithPath:[[NSString documentsPath] stringByAppendingPathComponent:@"睿智企业家.epub"]];
     
     [[VTReaderManager sharedInstance] openReaderWithPath:path type:VT_READER_TYPE_EPUB inViewController:self onDismiss:^BOOL(UIViewController *viewController) {
         
