5d70b8d5bd25febff39cdb3b256b8bb4894de394
vtears <772326244@qq.com>
2018-07-09 17:41:36 +0800

    修改弹出菜单界面为ReadMenuView

--- VTReader/Classes/Controllers/VTReadMenuView.m
+++ VTReader/Classes/Controllers/VTReadMenuView.m
@@ -8,14 +8,103 @@
 
 #import "VTReadMenuView.h"
 
+#import "VTReadTopMenuView.h"
+#import "VTReadBottomMenuView.h"
+
+extern NSString *const kNotificationShowOrHideMenu;
+
+@interface VTReadMenuView()<UIGestureRecognizerDelegate>
+
+/** 顶部菜单背景 */
+@property (nonatomic, strong) UIView *topMenuBgView;
+/** 顶部菜单 */
+@property (nonatomic, strong) VTReadTopMenuView *topMenuView;
+/** 底部菜单背景 */
+@property (nonatomic, strong) UIView *bottomMenuBgView;
+/** 底部菜单 */
+@property (nonatomic, strong) VTReadBottomMenuView *bottomMenuView;
+
+
+@end
+
 @implementation VTReadMenuView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        
+        [self setupSubviews];
+        
+//        UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTapGesture:)];
+////        tap.delegate = self;
+//        [self addGestureRecognizer:tap];
+    }
+    
+    return self;
 }
-*/
+
+- (void)setupSubviews
+{
+    CGFloat originX = 0;
+    CGFloat originY = 0;
+    CGFloat width = CGRectGetWidth(self.frame);
+    CGFloat height = [NSString isIphoneX] ? 88 : 64; // iPhoneX StatusBar高44px，NavigationBar高44px，底部TabBar高83px
+    // 普通 StatusBar高20px，NavigationBar高44px，底部TabBar高49px
+    
+    _topMenuBgView = [[UIView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+    self.topMenuBgView.backgroundColor = [UIColor colorForReaderMenu];
+//    self.topMenuBgView.hidden = YES;
+    
+    originY = [NSString isIphoneX] ? 44 : 20;
+    height = CGRectGetHeight(self.topMenuBgView.frame) - originY;
+    _topMenuView = [[VTReadTopMenuView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+    [self.topMenuBgView addSubview:self.topMenuView];
+    
+    height = [NSString isIphoneX] ? 220 + 34 : 220;
+    originY = CGRectGetHeight(self.frame) - height;
+    
+    _bottomMenuBgView = [[UIView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+    self.bottomMenuBgView.backgroundColor = [UIColor colorForReaderMenu];
+//    self.bottomMenuBgView.hidden = YES;
+    
+    originY = 0;
+    height = [NSString isIphoneX] ? CGRectGetHeight(self.bottomMenuBgView.frame) - 34 : CGRectGetHeight(self.bottomMenuBgView.frame);
+    _bottomMenuView = [[VTReadBottomMenuView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+    [self.bottomMenuBgView addSubview:self.bottomMenuView];
+    
+    [self addSubview:self.topMenuBgView];
+    [self addSubview:self.bottomMenuBgView];
+}
+
+#pragma mark - SEL
+- (void)handleTapGesture:(UITapGestureRecognizer *)recognizer
+{
+//    CGPoint point = [recognizer locationInView:self];
+//    // 判断是否点击到中间区域
+//    CGRect centerRect = CGRectMake(self.center.x - 20, 0, 40, CGRectGetHeight(self.frame));
+//    if (CGRectContainsPoint(centerRect, point)) {
+//        // 点击到中间区域, 展示上下菜单栏
+//        [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationShowOrHideMenu object:nil];
+//    }
+        [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationShowOrHideMenu object:nil];
+}
+
+//#pragma mark - UIGestureRecognizerDelegate
+////- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer
+//- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch
+//{
+//    CGPoint point = [touch locationInView:self];
+//    // 判断是否点击到中间区域
+//    CGRect centerRect = CGRectMake(self.center.x - 20, 0, 40, CGRectGetHeight(self.frame));
+//    if (CGRectContainsPoint(centerRect, point)) {
+//        // 点击到中间区域, 展示上下菜单栏
+//        //        [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationCloseReader object:nil];
+//        //        [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationShowOrHideMenu object:nil];
+//        return YES;
+//    }
+//
+//    return NO;
+//}
 
 @end
--- VTReader/Classes/Controllers/VTReadViewController.m
+++ VTReader/Classes/Controllers/VTReadViewController.m
@@ -12,8 +12,7 @@
 
 #import "VTReaderConfig.h"
 
-#import "VTReadTopMenuView.h"
-#import "VTReadBottomMenuView.h"
+#import "VTReadMenuView.h"
 
 #import "NSString+VTPhoneInfo.h"
 
@@ -27,14 +26,17 @@ NSString *const kNotificationShowOrHideMenu = @"kNotificationShowOrHideMenu"; //
 /** 翻页控制器的数据源代理 */
 @property (nonatomic, strong) VTReadModelController *modelController;
 
-/** 顶部菜单背景 */
-@property (nonatomic, strong) UIView *topMenuBgView;
-/** 顶部菜单 */
-@property (nonatomic, strong) VTReadTopMenuView *topMenuView;
-/** 底部菜单背景 */
-@property (nonatomic, strong) UIView *bottomMenuBgView;
-/** 底部菜单 */
-@property (nonatomic, strong) VTReadBottomMenuView *bottomMenuView;
+///** 顶部菜单背景 */
+//@property (nonatomic, strong) UIView *topMenuBgView;
+///** 顶部菜单 */
+//@property (nonatomic, strong) VTReadTopMenuView *topMenuView;
+///** 底部菜单背景 */
+//@property (nonatomic, strong) UIView *bottomMenuBgView;
+///** 底部菜单 */
+//@property (nonatomic, strong) VTReadBottomMenuView *bottomMenuView;
+
+/** 菜单界面 */
+@property (nonatomic, strong) VTReadMenuView *menuView;
 
 /** statusBar的显示和隐藏 */
 @property (nonatomic) BOOL hiddenStatusBar;
@@ -51,6 +53,15 @@ NSString *const kNotificationShowOrHideMenu = @"kNotificationShowOrHideMenu"; //
     }
     
     return _modelController;
+}
+
+- (VTReadMenuView *)menuView
+{
+    if (!_menuView) {
+        _menuView = [[VTReadMenuView alloc] initWithFrame:self.contentView.bounds];
+    }
+    
+    return _menuView;
 }
 
 #pragma mark - 生命周期
@@ -101,35 +112,37 @@ NSString *const kNotificationShowOrHideMenu = @"kNotificationShowOrHideMenu"; //
 #pragma mark - 添加菜单视图
 - (void)addMenuView
 {
-    CGFloat originX = 0;
-    CGFloat originY = 0;
-    CGFloat width = CGRectGetWidth(self.view.frame);
-    CGFloat height = [NSString isIphoneX] ? 88 : 64; // iPhoneX StatusBar高44px，NavigationBar高44px，底部TabBar高83px
-                                                                        // 普通 StatusBar高20px，NavigationBar高44px，底部TabBar高49px
-    
-    _topMenuBgView = [[UIView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
-    self.topMenuBgView.backgroundColor = [UIColor colorForReaderMenu];
-    self.topMenuBgView.hidden = YES;
-    
-    originY = [NSString isIphoneX] ? 44 : 20;
-    height = CGRectGetHeight(self.topMenuBgView.frame) - originY;
-    _topMenuView = [[VTReadTopMenuView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
-    [self.topMenuBgView addSubview:self.topMenuView];
-    
-    height = [NSString isIphoneX] ? 220 + 34 : 220;
-    originY = CGRectGetHeight(self.view.frame) - height;
-    
-    _bottomMenuBgView = [[UIView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
-    self.bottomMenuBgView.backgroundColor = [UIColor colorForReaderMenu];
-    self.bottomMenuBgView.hidden = YES;
-    
-    originY = 0;
-    height = [NSString isIphoneX] ? CGRectGetHeight(self.bottomMenuBgView.frame) - 34 : CGRectGetHeight(self.bottomMenuBgView.frame);
-    _bottomMenuView = [[VTReadBottomMenuView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
-    [self.bottomMenuBgView addSubview:self.bottomMenuView];
-    
-    [self.contentView addSubview:self.topMenuBgView];
-    [self.contentView addSubview:self.bottomMenuBgView];
+//    CGFloat originX = 0;
+//    CGFloat originY = 0;
+//    CGFloat width = CGRectGetWidth(self.view.frame);
+//    CGFloat height = [NSString isIphoneX] ? 88 : 64; // iPhoneX StatusBar高44px，NavigationBar高44px，底部TabBar高83px
+//                                                                        // 普通 StatusBar高20px，NavigationBar高44px，底部TabBar高49px
+//
+//    _topMenuBgView = [[UIView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+//    self.topMenuBgView.backgroundColor = [UIColor colorForReaderMenu];
+//    self.topMenuBgView.hidden = YES;
+//
+//    originY = [NSString isIphoneX] ? 44 : 20;
+//    height = CGRectGetHeight(self.topMenuBgView.frame) - originY;
+//    _topMenuView = [[VTReadTopMenuView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+//    [self.topMenuBgView addSubview:self.topMenuView];
+//
+//    height = [NSString isIphoneX] ? 220 + 34 : 220;
+//    originY = CGRectGetHeight(self.view.frame) - height;
+//
+//    _bottomMenuBgView = [[UIView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+//    self.bottomMenuBgView.backgroundColor = [UIColor colorForReaderMenu];
+//    self.bottomMenuBgView.hidden = YES;
+//
+//    originY = 0;
+//    height = [NSString isIphoneX] ? CGRectGetHeight(self.bottomMenuBgView.frame) - 34 : CGRectGetHeight(self.bottomMenuBgView.frame);
+//    _bottomMenuView = [[VTReadBottomMenuView alloc] initWithFrame:CGRectMake(originX, originY, width, height)];
+//    [self.bottomMenuBgView addSubview:self.bottomMenuView];
+//
+//    [self.contentView addSubview:self.topMenuBgView];
+//    [self.contentView addSubview:self.bottomMenuBgView];
+    self.menuView.hidden = YES;
+    [self.contentView addSubview:self.menuView];
 }
 
 #pragma mark - Override
@@ -141,17 +154,29 @@ NSString *const kNotificationShowOrHideMenu = @"kNotificationShowOrHideMenu"; //
 #pragma mark - NSNotification 通知展示菜单
 - (void)onNotificationShowMenu:(NSNotification *)notification
 {
+//    CATransition *animation = [CATransition animation];
+//    animation.type = kCATransitionFade;
+//    animation.duration = 0.5;
+//    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+//    [self.topMenuBgView.layer addAnimation:animation forKey:nil] ;
+//    [self.bottomMenuBgView.layer addAnimation:animation forKey:nil];
+//
+//    self.topMenuBgView.hidden = !self.topMenuBgView.hidden;
+//    self.hiddenStatusBar = self.topMenuBgView.hidden;
+//    [self setNeedsStatusBarAppearanceUpdate];
+//    self.bottomMenuBgView.hidden = !self.bottomMenuBgView.hidden;
+    
     CATransition *animation = [CATransition animation];
     animation.type = kCATransitionFade;
     animation.duration = 0.5;
     animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-    [self.topMenuBgView.layer addAnimation:animation forKey:nil] ;
-    [self.bottomMenuBgView.layer addAnimation:animation forKey:nil];
+    [self.menuView.layer addAnimation:animation forKey:nil];
     
-    self.topMenuBgView.hidden = !self.topMenuBgView.hidden;
-    self.hiddenStatusBar = self.topMenuBgView.hidden;
+    self.menuView.hidden = !self.menuView.hidden;
+    self.hiddenStatusBar = self.menuView.hidden;
     [self setNeedsStatusBarAppearanceUpdate];
-    self.bottomMenuBgView.hidden = !self.bottomMenuBgView.hidden;
+    
+    
     
 }
 
