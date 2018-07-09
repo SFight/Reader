2686a84e58ebd49651b725db903f6a632b2af22a
vtears <772326244@qq.com>
2018-07-09 15:55:30 +0800

    添加top的返回按钮

--- VTReader/Classes/Controllers/VTReadTopMenuView.m
+++ VTReader/Classes/Controllers/VTReadTopMenuView.m
@@ -8,14 +8,34 @@
 
 #import "VTReadTopMenuView.h"
 
+extern NSString *const kNotificationCloseReader;
+
 @implementation VTReadTopMenuView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupSubviews];
+    }
+    
+    return self;
 }
-*/
+
+#pragma mark - 设置视图
+- (void)setupSubviews
+{
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0, 0, CGRectGetHeight(self.frame), CGRectGetHeight(self.frame));
+    [button setImage:[UIImage imageNamed:@"vt_back" inBundle:[NSBundle bundleForClass:[self class]] compatibleWithTraitCollection:nil] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(onClickBackButton:) forControlEvents:UIControlEventTouchUpInside];
+    [self addSubview:button];
+}
+
+#pragma mark - SEL
+- (void)onClickBackButton:(UIButton *)button
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:kNotificationCloseReader object:nil];
+}
 
 @end
