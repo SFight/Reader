3bcfb8a00d13a376a511cdd158c11725e85ea90b
vtears <772326244@qq.com>
2018-07-09 11:09:14 +0800

    添加iPhoneX的判断扩展。

--- VTReader/Classes/Catagory/NSString+VTPhoneInfo.h
+++ VTReader/Classes/Catagory/NSString+VTPhoneInfo.h
@@ -8,6 +8,73 @@
 
 #import <Foundation/Foundation.h>
 
+/**
+ 手机型号枚举
+ */
+typedef NS_ENUM(NSInteger, kDeviceType) {
+    None_Type, // 设备类型未知
+    // 模拟器
+    Simulator, // Simulator
+    // AirPods
+    Air_Pods, // Air Pods
+    // Apple TV
+    Apple_TV_2ND_GENERATION, // Apple TV (2nd generation)
+    Apple_TV_3RD_GENERATION, // Apple TV (3rd generation)
+    Apple_TV_4TH_GENERATION, // APple TV (4th generation)
+    Apple_TV_4K, // Apple TV 4k
+    // AppleWatch
+    Apple_Watch_1ST_GENERATION, // Apple Watch (1st generation)
+    Apple_Watch_Series_1, // Apple Watch Series 1
+    Apple_Watch_Series_2, // Apple Watch Series 2
+    Apple_Watch_Series_3, // Apple Watch Series 3
+    // HomePod
+    Home_Pod, // Home Pod
+    // iPad
+    iPad, // iPad 768x1024 768x1024 @1x
+    iPad_2, // iPad 2 768x1024 768x1024 @1x
+    iPad_3RD_GENERATION, // iPad (3rd generation) 768x1024 1536x2048 @2x
+    iPad_4TH_GENERATION, // iPad (4th generation) 768x1024 1536x2048 @2x
+    iPad_5TH_GENERATION, // iPad (5th generation) 768x1024 1536x2048 @2x
+    iPad_6TH_GENERATION, // iPad (6th generation) 768x1024 1536x2048 @2x
+    iPad_Air, // iPad Air 768x1024 1536x2048 @2x
+    iPad_Air_2, // iPad Air 2 768x1024 1536x2048 @2x
+    iPad_Pro_9_7, // iPad Pro (9.7-inch) 768x1024 1536x2048 @2x
+    iPad_Pro_10_5, // iPad Pro (10.5-inch) 834x1112 1668x2224 @2x
+    iPad_Pro_12_9, // iPad Pro (12.9-inch) 1024x1336 2048x2732 @2x
+    iPad_Pro_12_9_2ND, // iPad Pro (12.9-inch, 2nd generation) 1024x1336 2048x2732 @2x
+    // iPad mini
+    iPad_MINI, // iPad mini 768x1024 768x1024 @1x
+    iPad_MINI_2, // iPad mini 2 768x1024 1536x2048 @2x
+    iPad_MINI_3, // iPad mini 3 768x1024 1536x2048 @2x
+    iPad_MINI_4, // iPad mini 4 768x1024 1536x2048 @2x
+    // iPhone
+    iPhone_1G, // iPhone 1G 320x480 320x480 @1x
+    iPhone_3G, // iPhone 3G 320x480 320x480 @1x
+    iPhone_3GS, // iPhone 3GS 320x480 320x480 @1x
+    iPhone_4, // iPhone 4 320x480 640x960 @2x
+    iPhone_4S, // iPhone 4S 320x480 640x960 @2x
+    iPhone_5, // iPhone 5 320x568 640x1136 @2x
+    iPhone_5C, // iPhone 5C 320x568 640x1136 @2x
+    iPhone_5S, // iPhone 5S 320x568 640x1136 @2x
+    iPhone_6, // iPhone 6 375x667 750x1334 @2x
+    iPhone_6_PLUS, // iPhone 6 Plus 414x736 (1242x2208)->1080x1920 @3x
+    iPhone_6S, // iPhone 6S 375x667 750x1134 @2x
+    iPhone_6S_PLUS, // iPhone 6S Plus 414x736 (1242x2208)->1080x1920 @3x
+    iPhone_SE, // iPhone SE 320x568 640x1136 @2x
+    iPhone_7, // iPhone 7 375x667 750x1134 @2x
+    iPhone_7_PLUS, // iPhone 7 Plus 414x736 (1242x2208)->1080x1920 @3x
+    iPhone_8, // iPhone 8 375x667 750x1134 @2x
+    iPhone_8_PLUS, // iPhone_8_PLUS 414x736 (1242x2208)->1080x1920 @3x
+    iPhone_X, // iPhone_X 375x812 1125x2436 @3x
+    // iPod touch
+    iPod_TOUCH, // iPod touch 320x480 320x480 @1x
+    iPod_TOUCH_2ND_GENERATION, // iPod touch (2nd generation) 320x480 320x480 @1x
+    iPod_TOUCH_3RD_GENERATION, // iPod touch (3rd generation) 320x480 320x480 @1x
+    iPod_TOUCH_4TH_GENERATION, // iPod touch (4th generation) 320x480 640x960 @2x
+    iPod_TOUCH_5TH_GENREATION, // iPod touch (5th generation) 320x568 640x1136 @2x
+    iPod_TOUCH_6TH_GENERATION // iPod touch (6th generation) 320x568 640x1136 @2x
+};
+
 @interface NSString (VTPhoneInfo)
 
 #pragma mark - 获取手机序列号
@@ -36,5 +103,19 @@
 
 #pragma mark - 获取当前应用版本号码
 - (NSString *)appCurVersionNum;
+
+/**
+ 判断当前手机的型号为4、4s、5、5s、6、6p、6s、6sp、7、7p、8、8p、x
+ 
+ @return  设备型号枚举类型kDeviceType
+ */
++ (kDeviceType)currentDeviceType;
+
+/**
+ 判断是否是iPhoneX
+ 
+ @return iPhoneX判断结果
+ */
++ (BOOL)isIphoneX;
 
 @end
--- VTReader/Classes/Catagory/NSString+VTPhoneInfo.m
+++ VTReader/Classes/Catagory/NSString+VTPhoneInfo.m
@@ -8,6 +8,8 @@
 
 #import "NSString+VTPhoneInfo.h"
 
+#import <sys/utsname.h>
+
 @implementation NSString (VTPhoneInfo)
 
 #pragma mark - 获取手机序列号
@@ -120,6 +122,142 @@
     VTLog(@"当前应用版本号码：%@",appCurVersionNum);
     
     return appCurVersionNum;
+}
+
+#pragma mark - 设备类型获取
++ (kDeviceType)currentDeviceType
+{
+    // 参考模型来源：https://www.theiphonewiki.com/wiki/Models
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *deviceString = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+    
+    //simulator
+    if ([@"i386" isEqualToString:deviceString] || [@"x86_64" isEqualToString:deviceString]) return Simulator;
+    
+    //AirPods
+    if ([@"AirPods1,1" isEqualToString:deviceString]) {
+        return Air_Pods;
+    }
+    
+    //Apple TV
+    if ([@"AppleTV2,1" isEqualToString:deviceString])    return Apple_TV_2ND_GENERATION;
+    if ([@"AppleTV3,1" isEqualToString:deviceString])    return Apple_TV_3RD_GENERATION;
+    if ([@"AppleTV3,2" isEqualToString:deviceString])    return Apple_TV_3RD_GENERATION;
+    if ([@"AppleTV5,3" isEqualToString:deviceString])    return Apple_TV_4TH_GENERATION;
+    if ([@"AppleTV6,2" isEqualToString:deviceString])    return Apple_TV_4K;
+    
+    //Apple Watch
+    if ([@"Watch1,1" isEqualToString:deviceString])    return Apple_Watch_1ST_GENERATION;
+    if ([@"Watch1,2" isEqualToString:deviceString])    return Apple_Watch_1ST_GENERATION;
+    if ([@"Watch2,6" isEqualToString:deviceString])    return Apple_Watch_Series_1;
+    if ([@"Watch2,7" isEqualToString:deviceString])    return Apple_Watch_Series_1;
+    if ([@"Watch2,3" isEqualToString:deviceString])    return Apple_Watch_Series_2;
+    if ([@"Watch2,4" isEqualToString:deviceString])    return Apple_Watch_Series_2;
+    if ([@"Watch3,1" isEqualToString:deviceString])    return Apple_Watch_Series_3;
+    if ([@"Watch3,2" isEqualToString:deviceString])    return Apple_Watch_Series_3;
+    if ([@"Watch3,3" isEqualToString:deviceString])    return Apple_Watch_Series_3;
+    if ([@"Watch3,4" isEqualToString:deviceString])    return Apple_Watch_Series_3;
+    
+    //HomePod
+    if ([@"AudioAccessory1,1" isEqualToString:deviceString])    return Home_Pod;
+    
+    //iPad
+    if ([@"iPad1,1" isEqualToString:deviceString])    return iPad;
+    if ([@"iPad2,1" isEqualToString:deviceString])    return iPad_2;
+    if ([@"iPad2,2" isEqualToString:deviceString])    return iPad_2;
+    if ([@"iPad2,3" isEqualToString:deviceString])    return iPad_2;
+    if ([@"iPad2,4" isEqualToString:deviceString])    return iPad_2;
+    if ([@"iPad3,1" isEqualToString:deviceString])    return iPad_3RD_GENERATION;
+    if ([@"iPad3,2" isEqualToString:deviceString])    return iPad_3RD_GENERATION;
+    if ([@"iPad3,3" isEqualToString:deviceString])    return iPad_3RD_GENERATION;
+    if ([@"iPad3,4" isEqualToString:deviceString])    return iPad_4TH_GENERATION;
+    if ([@"iPad3,5" isEqualToString:deviceString])    return iPad_4TH_GENERATION;
+    if ([@"iPad3,6" isEqualToString:deviceString])    return iPad_4TH_GENERATION;
+    if ([@"iPad4,1" isEqualToString:deviceString])    return iPad_Air;
+    if ([@"iPad4,2" isEqualToString:deviceString])    return iPad_Air;
+    if ([@"iPad4,3" isEqualToString:deviceString])    return iPad_Air;
+    if ([@"iPad5,3" isEqualToString:deviceString])    return iPad_Air_2;
+    if ([@"iPad5,4" isEqualToString:deviceString])    return iPad_Air_2;
+    if ([@"iPad6,7" isEqualToString:deviceString])    return iPad_Pro_12_9;
+    if ([@"iPad6,8" isEqualToString:deviceString])    return iPad_Pro_12_9;
+    if ([@"iPad6,3" isEqualToString:deviceString])    return iPad_Pro_9_7;
+    if ([@"iPad6,4" isEqualToString:deviceString])    return iPad_Pro_9_7;
+    if ([@"iPad6,11" isEqualToString:deviceString])    return iPad_5TH_GENERATION;
+    if ([@"iPad6,12" isEqualToString:deviceString])    return iPad_5TH_GENERATION;
+    if ([@"iPad7,1" isEqualToString:deviceString])    return iPad_Pro_12_9_2ND;
+    if ([@"iPad7,2" isEqualToString:deviceString])    return iPad_Pro_12_9_2ND;
+    if ([@"iPad7,3" isEqualToString:deviceString])    return iPad_Pro_10_5;
+    if ([@"iPad7,4" isEqualToString:deviceString])    return iPad_Pro_10_5;
+    if([@"iPad7,5" isEqualToString:deviceString])     return iPad_6TH_GENERATION;
+    if([@"iPad7,6" isEqualToString:deviceString])     return iPad_6TH_GENERATION;
+    
+    //iPad mini
+    if ([@"iPad2,5" isEqualToString:deviceString])    return iPad_MINI;
+    if ([@"iPad2,6" isEqualToString:deviceString])    return iPad_MINI;
+    if ([@"iPad2,7" isEqualToString:deviceString])    return iPad_MINI;
+    if ([@"iPad4,4" isEqualToString:deviceString])    return iPad_MINI_2;
+    if ([@"iPad4,5" isEqualToString:deviceString])    return iPad_MINI_2;
+    if ([@"iPad4,6" isEqualToString:deviceString])    return iPad_MINI_2;
+    if ([@"iPad4,7" isEqualToString:deviceString])    return iPad_MINI_3;
+    if ([@"iPad4,8" isEqualToString:deviceString])    return iPad_MINI_3;
+    if ([@"iPad4,9" isEqualToString:deviceString])    return iPad_MINI_3;
+    if ([@"iPad5,1" isEqualToString:deviceString])    return iPad_MINI_4;
+    if ([@"iPad5,2" isEqualToString:deviceString])    return iPad_MINI_4;
+    
+    //iPhone
+    if ([@"iPhone1,1" isEqualToString:deviceString])     return iPhone_1G;
+    if ([@"iPhone1,2" isEqualToString:deviceString])     return iPhone_3G;
+    if ([@"iPhone2,1" isEqualToString:deviceString])     return iPhone_3GS;
+    if ([@"iPhone3,1" isEqualToString:deviceString])     return iPhone_4;
+    if ([@"iPhone3,2" isEqualToString:deviceString])     return iPhone_4;
+    if([@"iPhone3,3" isEqualToString:deviceString])      return iPhone_4;
+    if ([@"iPhone4,1" isEqualToString:deviceString])     return iPhone_4S;
+    if ([@"iPhone5,1" isEqualToString:deviceString])     return iPhone_5;
+    if ([@"iPhone5,2" isEqualToString:deviceString])     return iPhone_5;
+    if ([@"iPhone5,3" isEqualToString:deviceString])     return iPhone_5C;
+    if ([@"iPhone5,4" isEqualToString:deviceString])     return iPhone_5C;
+    if ([@"iPhone6,1" isEqualToString:deviceString])     return iPhone_5S;
+    if ([@"iPhone6,2" isEqualToString:deviceString])     return iPhone_5S;
+    if ([@"iPhone7,2" isEqualToString:deviceString])     return iPhone_6;
+    if ([@"iPhone7,1" isEqualToString:deviceString])     return iPhone_6_PLUS;
+    if ([@"iPhone8,1" isEqualToString:deviceString])     return iPhone_6S;
+    if ([@"iPhone8,2" isEqualToString:deviceString])     return iPhone_6S_PLUS;
+    if ([@"iPhone8,4" isEqualToString:deviceString])     return iPhone_SE;
+    if ([@"iPhone9,1" isEqualToString:deviceString])     return iPhone_7;
+    if ([@"iPhone9,3" isEqualToString:deviceString])     return iPhone_7;
+    if ([@"iPhone9,2" isEqualToString:deviceString])     return iPhone_7_PLUS;
+    if ([@"iPhone9,4" isEqualToString:deviceString])     return iPhone_7_PLUS;
+    if ([@"iPhone10,1" isEqualToString:deviceString])    return iPhone_8;
+    if ([@"iPhone10,4" isEqualToString:deviceString])    return iPhone_8;
+    if ([@"iPhone10,2" isEqualToString:deviceString])    return iPhone_8_PLUS;
+    if ([@"iPhone10,5" isEqualToString:deviceString])    return iPhone_8_PLUS;
+    if ([@"iPhone10,3" isEqualToString:deviceString])    return iPhone_X;
+    if ([@"iPhone10,6" isEqualToString:deviceString])    return iPhone_X;
+    
+    //iPod touch
+    if ([@"iPod1,1" isEqualToString:deviceString])    return iPod_TOUCH;
+    if ([@"iPod2,1" isEqualToString:deviceString])    return iPod_TOUCH_2ND_GENERATION;
+    if ([@"iPod3,1" isEqualToString:deviceString])    return iPod_TOUCH_3RD_GENERATION;
+    if ([@"iPod4,1" isEqualToString:deviceString])    return iPod_TOUCH_4TH_GENERATION;
+    if ([@"iPod5,1" isEqualToString:deviceString])    return iPod_TOUCH_5TH_GENREATION;
+    if ([@"iPod7,1" isEqualToString:deviceString])    return iPod_TOUCH_6TH_GENERATION;
+    
+    return None_Type;
+}
+
++ (BOOL)isIphoneX
+{
+    kDeviceType deviceType = [self currentDeviceType];
+    if (deviceType == Simulator) {
+        // 模拟器根据屏幕尺寸判断iPhoneX
+        CGRect nativeBounds = [[UIScreen mainScreen] nativeBounds];
+        if(CGSizeEqualToSize(CGSizeMake(1125.f, 2436.f),  nativeBounds.size)) return YES;
+    }
+    
+    if (deviceType == iPhone_X) return YES;
+    
+    return NO;
 }
 
 @end
